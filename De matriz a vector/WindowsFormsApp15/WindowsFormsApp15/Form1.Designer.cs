1b9d22f16f6dc06e936191dd70a5615d68d19a90
Jose Nuñez <jnunez@serexrd.lo>
2018-08-17 10:19:24 -0400

    subida inicial del proyecto

---
+++ De matriz a vector/WindowsFormsApp15/WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vector:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox1.Location = new System.Drawing.Point(159, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(207, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(255, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 26);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox4.Location = new System.Drawing.Point(303, 160);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(42, 26);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox5.Location = new System.Drawing.Point(351, 160);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(42, 26);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox6.Location = new System.Drawing.Point(399, 160);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(42, 26);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox7.Location = new System.Drawing.Point(447, 160);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(42, 26);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox8.Location = new System.Drawing.Point(495, 160);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(42, 26);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(331, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Matriz";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox9.Location = new System.Drawing.Point(543, 160);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(42, 26);
+            this.textBox9.TabIndex = 10;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox13.Location = new System.Drawing.Point(287, 260);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(42, 26);
+            this.textBox13.TabIndex = 14;
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox14.Location = new System.Drawing.Point(335, 260);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(42, 26);
+            this.textBox14.TabIndex = 15;
+            this.textBox14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox15.Location = new System.Drawing.Point(383, 260);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(42, 26);
+            this.textBox15.TabIndex = 16;
+            this.textBox15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox17.Location = new System.Drawing.Point(287, 292);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(42, 26);
+            this.textBox17.TabIndex = 18;
+            this.textBox17.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox18
+            // 
+            this.textBox18.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox18.Location = new System.Drawing.Point(335, 292);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(42, 26);
+            this.textBox18.TabIndex = 19;
+            this.textBox18.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox19
+            // 
+            this.textBox19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox19.Location = new System.Drawing.Point(383, 292);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(42, 26);
+            this.textBox19.TabIndex = 20;
+            this.textBox19.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox21
+            // 
+            this.textBox21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox21.Location = new System.Drawing.Point(287, 324);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(42, 26);
+            this.textBox21.TabIndex = 22;
+            this.textBox21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox22
+            // 
+            this.textBox22.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox22.Location = new System.Drawing.Point(335, 324);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(42, 26);
+            this.textBox22.TabIndex = 23;
+            this.textBox22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox23
+            // 
+            this.textBox23.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox23.Location = new System.Drawing.Point(383, 324);
+            this.textBox23.Name = "textBox23";
+            this.textBox23.Size = new System.Drawing.Size(42, 26);
+            this.textBox23.TabIndex = 24;
+            this.textBox23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(75, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 40);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "De Vector a Matriz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(267, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(182, 40);
+            this.button2.TabIndex = 30;
+            this.button2.Text = "De Matriz a Vector";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(465, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 40);
+            this.button3.TabIndex = 31;
+            this.button3.Text = "Limpiar Valores";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(267, 462);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 40);
+            this.button4.TabIndex = 32;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(529, 87);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 40);
+            this.button5.TabIndex = 33;
+            this.button5.Text = "Creador por:";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(131, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(484, 31);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "De Matriz a Vector / De Vector a Matriz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(740, 541);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox23);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "De Vector a Matriz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ De matriz a vector/WindowsFormsApp15/WindowsFormsApp15/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Este programa esta bajo licencia MIT
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int A, B, C, D, E, F, G, H, I;
+
+            if (textBox1.Text == ""|| textBox2.Text == "" ||
+            textBox3.Text == "" || textBox4.Text == "" ||
+            textBox5.Text == "" || textBox6.Text == "" ||
+            textBox7.Text == "" || textBox8.Text == "" ||
+            textBox9.Text == "")
+                {
+
+                MessageBox.Show("Por favor, no debe dejar casillas vacias en esta operación");
+                return;
+            }
+
+
+
+
+
+            A = Convert.ToInt16(textBox1.Text); F = Convert.ToInt16(textBox6.Text);
+            B = Convert.ToInt16(textBox2.Text); G = Convert.ToInt16(textBox7.Text);
+            C = Convert.ToInt16(textBox3.Text); H = Convert.ToInt16(textBox8.Text);
+            D = Convert.ToInt16(textBox4.Text); I = Convert.ToInt16(textBox9.Text);
+            E = Convert.ToInt16(textBox5.Text);
+
+
+            int[] V = { A, B, C, D, E, F, G, H, I };
+            int[,] R = new int[3, 3]; //Este la matriz que recibirá los valores del vector.
+            int W = -1;
+
+            //De vector a matriz
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int columna = 0; columna < 3; columna++)
+                {
+                    W = W + 1;
+                    R[fila, columna] = V[W];
+
+                    textBox13.Text = Convert.ToString(R[0, 0]);
+                    textBox14.Text = Convert.ToString(R[0, 1]);
+                    textBox15.Text = Convert.ToString(R[0, 2]);
+
+                    textBox17.Text = Convert.ToString(R[1, 0]);
+                    textBox18.Text = Convert.ToString(R[1, 1]);
+                    textBox19.Text = Convert.ToString(R[1, 2]);
+
+                    textBox21.Text = Convert.ToString(R[2, 0]);
+                    textBox22.Text = Convert.ToString(R[2, 1]);
+                    textBox23.Text = Convert.ToString(R[2, 2]);
+
+                    textBox1.Text = ""; textBox2.Text = "";
+                    textBox3.Text = ""; textBox4.Text = "";
+                    textBox5.Text = ""; textBox6.Text = "";
+                    textBox7.Text = ""; textBox8.Text = "";
+                    textBox9.Text = "";
+
+                }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int A, B, C, D, E, F, G, H, I;
+
+
+            if (textBox13.Text == "" || textBox14.Text == "" ||
+           textBox15.Text == "" || textBox21.Text == "" ||
+           textBox17.Text == "" || textBox22.Text == "" ||
+           textBox18.Text == "" || textBox23.Text == "" ||
+           textBox19.Text == "")
+            {
+
+                MessageBox.Show("Por favor, no debe dejar casillas vacias en esta operación");
+                return;
+            }
+
+
+
+            A = Convert.ToInt16(textBox13.Text); F = Convert.ToInt16(textBox19.Text);
+            B = Convert.ToInt16(textBox14.Text); G = Convert.ToInt16(textBox21.Text);
+            C = Convert.ToInt16(textBox15.Text); H = Convert.ToInt16(textBox22.Text);
+            D = Convert.ToInt16(textBox17.Text); I = Convert.ToInt16(textBox23.Text);
+            E = Convert.ToInt16(textBox18.Text);
+
+            int[,] R = { { A, B, C }, { D, E, F }, { G, H, I } };
+            int[] V = new int[9];
+            int W = -1;
+
+            for (int fila = 0; fila < 3; fila++)
+            {
+                for (int columna = 0; columna < 3; columna++)
+                {
+                    W = W + 1;
+                     V[W] = R[fila, columna];
+
+                    textBox1.Text = Convert.ToString(V[0]);
+                    textBox2.Text = Convert.ToString(V[1]);
+                    textBox3.Text = Convert.ToString(V[2]);
+
+                    textBox4.Text = Convert.ToString(V[3]);
+                    textBox5.Text = Convert.ToString(V[4]);
+                    textBox6.Text = Convert.ToString(V[5]);
+
+                    textBox7.Text = Convert.ToString(V[6]);
+                    textBox8.Text = Convert.ToString(V[7]);
+                    textBox9.Text = Convert.ToString(V[8]);
+
+                    textBox13.Text = ""; textBox14.Text = "";
+                    textBox15.Text = ""; textBox21.Text = "";
+                    textBox17.Text = ""; textBox22.Text = "";
+                    textBox18.Text = ""; textBox23.Text = "";
+                    textBox19.Text = "";
+
+
+                }
+
+            }            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ""; textBox2.Text = "";
+            textBox3.Text = ""; textBox4.Text = "";
+            textBox5.Text = ""; textBox6.Text = "";
+            textBox7.Text = ""; textBox8.Text = "";
+            textBox9.Text = "";
+
+
+            textBox13.Text = ""; textBox14.Text = "";
+            textBox15.Text = ""; textBox17.Text = "";
+            textBox18.Text = ""; textBox19.Text = "";
+            textBox21.Text = ""; textBox22.Text = "";
+            textBox23.Text = "";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jose Luis Nuñez - Matricula: 00-MISN-1-198");
+        }
+        //Este programa esta bajo licencia MIT
+    }
+}
